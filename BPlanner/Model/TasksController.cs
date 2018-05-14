70e45b73c9a6e9eac61bfa5110c78b084ea5d01a
Vladyslav Boiko <vladboyko3112@gmail.com>
2018-05-14 21:41:09 +0300

    Task type added

---
+++ BPlanner/Model/TasksController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Model
+{
+    [Serializable]
+    public class TasksController
+    {
+        private static TasksController tasksController = null;
+
+        
+        private List<CalendarTask> everyday;
+        private TasksController()
+        {
+            Task.WaitAll();
+        }
+
+        public static TasksController GetTasksController()
+        {
+            if (tasksController != null)
+                return tasksController;
+
+            TasksController temp = new TasksController();
+            Interlocked.CompareExchange(ref tasksController, temp, null);
+            return tasksController;
+        }
+    }
+}
