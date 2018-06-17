635c8be3248be9a668083c17c22626596e2853ce
Vladyslav Boiko <vladboyko3112@gmail.com>
2018-06-17 20:37:58 +0300

    DAL created

---
+++ BPlanner/DAL/SerializationDA.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    abstract class SerializationDA : IDataAccess
+    {
+        protected readonly string pathForFile;
+
+        protected SerializationDA(string nameOfClass)
+        {
+            pathForFile = GetPathForFile() + @"\" + nameOfClass + ".dat";
+        }
+
+        private string GetPathForFile()
+        {
+            string nameOfDirectory = @"\Data";
+            DirectoryInfo directoryInfo = null;
+            string path = null;
+
+            if (Directory.Exists(Directory.GetCurrentDirectory() + nameOfDirectory))
+                directoryInfo = Directory.CreateDirectory(Directory.GetCurrentDirectory() + nameOfDirectory);
+            else
+                path = Directory.GetCurrentDirectory() + nameOfDirectory;
+
+            return path ?? directoryInfo.FullName;
+        }
+
+        public abstract object GetData();
+
+        public abstract void SetData(Object data);
+    }
+}
